47d332653380e3b64b9175b32422a7f28f42ca96
mwest0101 <mauriciowest@gmail.com>
2023-08-13 16:41:06 -0300

    Agregando funcion de encendido de pantalla

--- EEST2BigScreenMatrixScreen.ino
+++ EEST2BigScreenMatrixScreen.ino
@@ -6,19 +6,26 @@ static const int PROGMEM t2[]={ 1,1,1,0,1,1,1,
                                 0,1,0,0,1,0,0,
                                 0,1,0,0,1,1,1,-1};
 */                            
-static const int PROGMEM t2[]={ I,I,I,O,I,I,I,EL,
+static const int PROGMEM C_T2[]={ I,I,I,O,I,I,I,EL,
                                 O,I,O,O,O,O,I,EL,
                                 O,I,O,O,I,I,I,EL,
                                 O,I,O,O,I,O,O,EL,
                                 O,I,O,O,I,I,I,EA};                                
-                              
+
+static int * aFrame;
+static int * aPins;
+int nFrameElements=0;
+int pinState=0;
+
 void setup() {
   Serial.begin(9600);
   ShowMatrix sm(10);
   
-  static int * aEffect;
+  
  
-  aEffect=convProgToArray(t2,(sizeof(t2)/2));
+  aFrame=convProgToArray(C_T2,(sizeof(C_T2)/2));
+  aPins=convProgToArray(C_Pins,(sizeof(C_Pins)/2));
+  nFrameElements = sizeof(aPins) / sizeof(int);
 
   /*
   aPines=convProgToArray(C_PANEL_PIN,(sizeof(C_PANEL_PIN)/2));
@@ -29,15 +36,23 @@ void setup() {
   for (int i = 0; i < nPines; i++) {
     pinMode(aEffect[i], OUTPUT);
   }*/
+
+  for (int i = 0; i < nFrameElements; i++) {
+    pinMode(aPins[i], OUTPUT);
+  }
 }
 // Secuencia de la matriz
 void loop() {
-  /*
-  for (int i = 0; i < nEffects; i++) {
-    elemento = aPines[i];
-    digitalWrite(aEffect[i], elemento);
+  
+  for (int i = 0; i < nFrameElements; i++) {
+    pinState = aFrame[i];
+    if(pinState<2){
+      digitalWrite(aPins[i], pinState);
+      Serial.println(aPins[i]);
+      Serial.println(pinState);
+    }
   }
   delay(1000);
-  */
+  
  
 }
--- inc_config.h
+++ inc_config.h
@@ -2,7 +2,7 @@
 
 #ifndef INC_CONFIG_H
 #define INC_CONFIG_H
-   //int panel[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37};
+   
    
    #define C_PIN_ENABLED  1
    #define C_PIN_DISABLED 0
@@ -14,20 +14,20 @@
    #define EA             C_END_ARR
    #define EL             C_END_LIN
 
-/*
-    static const int PROGMEM CPanel[]={ 
-      2,7,12,17,22,27,32,
-      3,8,13,18,23,28,33,
-      4,9,14,19,24,29,34,
-      5,10,15,20,25,30,35,
-      6,11,16,21,26,31,36,EA}
-*/
+  //static const int PROGMEM C_Pins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37};
 
-                              
-static const int PROGMEM t3[]={ I,I,I,O,I,I,I,
-                                O,I,O,O,O,O,I,
-                                O,I,O,O,I,I,I,
-                                O,I,O,O,I,O,O,
-                                O,I,O,O,I,I,I,EA};
+  static const int PROGMEM C_Pins[]={ 2,7,12,17,22,27,32,
+                                      3,8,13,18,23,28,33,
+                                      4,9,14,19,24,29,34,
+                                      5,10,15,20,25,30,35,
+                                      6,11,16,21,26,31,36,EA};
+
+
+                              /*
+  static const int PROGMEM t3[]={ I,I,I,O,I,I,I,
+                                  O,I,O,O,O,O,I,
+                                  O,I,O,O,I,I,I,
+                                  O,I,O,O,I,O,O,
+                                  O,I,O,O,I,I,I,EA};*/
 
 #endif /* INC_CONFIG_H */
