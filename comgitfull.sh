3985403a7c8768c4ab440eebfcce69fb41ac9ba1
mwest0101 <mauriciowest@gmail.com>
2023-10-30 03:18:49 -0300

    test3

--- comgitfull.sh
+++ comgitfull.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Solicitar el nombre del archivo al usuario
 read -p "Ingrese el nombre del archivo: " nombre_archivo
+archivo_comprimido="${nombre_archivo}.tar.gz"
+
 
 
 # Agregar archivos al repositorio Git
 git add .
-git commit -m "Agregado archivo comprimido $archivo_comprimido"
+git commit -m "${nombre_archivo}"
 git push
 
-archivo_comprimido="${nombre_archivo}.tar.gz"
+
 
 # Comprimir archivos, excluyendo .git, .rar y .gz
 tar --exclude='*.git' --exclude='*.rar' --exclude='*.gz' -czvf "$archivo_comprimido"
