7545e787e9d8e0b8ebbf2a7383f1a58f89ea059e
mwest0101 <mauriciowest@gmail.com>
2023-10-30 03:12:04 -0300

    Agregado archivo comprimido

--- comgitfull.sh
+++ comgitfull.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-
 # Solicitar el nombre del archivo al usuario
 read -p "Ingrese el nombre del archivo: " nombre_archivo
+
+
+# Agregar archivos al repositorio Git
+git add .
+git commit -m "Agregado archivo comprimido $archivo_comprimido"
+git push
 
 archivo_comprimido="${nombre_archivo}.tar.gz"
 
 # Comprimir archivos, excluyendo .git, .rar y .gz
 tar --exclude='*.git' --exclude='*.rar' --exclude='*.gz' -czvf "$archivo_comprimido"
 
-# Agregar archivos al repositorio Git
-git add "$archivo_comprimido"
-
-# Realizar commit
-git commit -m "Agregado archivo comprimido $archivo_comprimido"
-
-# Hacer push
-git push
-
-echo "Proceso completado. Archivo comprimido: $archivo_comprimido, agregado al repositorio Git y se ha realizado el commit y push."
+echo "Proceso completado. Archivo comprimido y se ha realizado el commit y push."
