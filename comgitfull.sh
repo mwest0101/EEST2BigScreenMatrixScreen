876bd8fe56f1ffa1a8b9a467627f3fb0d41d4805
mwest0101 <mauriciowest@gmail.com>
2023-11-07 20:27:33 -0300

    test3

--- comgitfull.sh
+++ comgitfull.sh
@@ -1,23 +1,18 @@
-
-
-# Obtener la fecha y hora actual en un formato deseado (por ejemplo, YYYYMMDD_HHMMSS)
-fecha_hora_actual=$(date +'%Y%m%d_%H%M%S')
-
+#!/bin/bash
 # Solicitar el nombre del archivo al usuario
 read -p "Ingrese el nombre del archivo: " nombre_archivo
+archivo_comprimido="${nombre_archivo}.tar.gz"
 
-# Directorio donde se guardará el archivo comprimido
-ruta_destino="../"
 
-archivo_comprimido="${ruta_destino}/${nombre_archivo}_${fecha_hora_actual}.tar.gz"
 
 # Agregar archivos al repositorio Git
-
 git add .
 git commit -m "${nombre_archivo}"
 git push
 
-# Comprimir archivos, excluyendo .git, .rar y .gz
-tar --directory="${ruta_destino}" --exclude='*.git' --exclude='*.rar' --exclude='*.gz' -czvf "${archivo_comprimido}" *
 
-echo "Proceso completado. Archivo comprimido y se ha realizado el commit y push en la ruta ${ruta_destino}."
+
+# Comprimir archivos, excluyendo .git, .rar y .gz
+tar --exclude='*.git' --exclude='*.rar' --exclude='*.gz' --exclude='*.zip' -czvf "$archivo_comprimido" *
+
+echo "Proceso completado. Archivo comprimido y se ha realizado el commit y push."
